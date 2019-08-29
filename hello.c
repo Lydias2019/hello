2994880cd5719c86b37739d017acac6c4bf44613
Lydias2019 <54672695+Lydias2019@users.noreply.github.com>
2019-08-29 08:37:32 -0500

    Create hello.c
    Hello World file with Quinn and Lydia. Added a line printing my major.

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Quinn Reimer and Lydia Saltz
+ * Date: 2019/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Quinn and Lydia\n");
+
+
+  prinf("Lydia- Undeclared");
+
+  return 0;
+}
